3f58c549bfcc8a9b266b01b3410283397025717e
Sriraj Madhavan Potti <smadhavan@costar.com>
2021-01-12 20:11:16 -0500

    app added

---
+++ build/precache-manifest.795280eb62815832fcfceb6eab4250d8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "659a297d680bdd7aa99e2d3783df15ec",
+    "url": "/dboard-time/index.html"
+  },
+  {
+    "revision": "0b737aeca0f3794ba04d",
+    "url": "/dboard-time/static/css/2.7bfd32cb.chunk.css"
+  },
+  {
+    "revision": "fd2b82d931745a9354ee",
+    "url": "/dboard-time/static/css/main.2f865207.chunk.css"
+  },
+  {
+    "revision": "0b737aeca0f3794ba04d",
+    "url": "/dboard-time/static/js/2.e49325af.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/dboard-time/static/js/2.e49325af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "633559e0d69ff56a46b1",
+    "url": "/dboard-time/static/js/3.6d6c1bb4.chunk.js"
+  },
+  {
+    "revision": "fd2b82d931745a9354ee",
+    "url": "/dboard-time/static/js/main.8d6888ca.chunk.js"
+  },
+  {
+    "revision": "a6f13ad81d38ea4a8d71",
+    "url": "/dboard-time/static/js/runtime-main.1a684c3e.js"
+  }
+]);
--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,91 @@
-import 'antd/dist/antd.min.css';
-import './App.css';
-
 import { Link, Route, BrowserRouter as Router, Switch } from 'react-router-dom';
-import React, { Suspense, lazy } from 'react';
+import React, { useState } from "react";
 
 import { Divider } from 'antd';
 import HelloModal from './components/HelloModal';
 import Home from './pages/Home';
 import LibVersion from './components/LibVersion';
 
-const About = lazy(() => import('./pages/About'));
+// const About = lazy(() => import('./pages/About'));
 
-const RouteExample = () => {
-  return (
-    //@ts-ignore
-    <Router basename={window.__POWERED_BY_QIANKUN__ ? '/react16' : '/'}>
-      <nav>
-        <Link to="/">Home</Link>
-        <Divider type="vertical" />
-        <Link to="/about">About</Link>
-      </nav>
-      <Suspense fallback={null}>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/about" component={About} />
-        </Switch>
-      </Suspense>
-    </Router>
-  );
-};
+// const RouteExample = () => {
+//   return (
+//     //@ts-ignore
+//     <Router basename={window.__POWERED_BY_QIANKUN__ ? '/react16' : '/'}>
+//       <nav>
+//         <Link to="/">Home</Link>
+//         <Divider type="vertical" />
+//         <Link to="/about">About</Link>
+//       </nav>
+//       <Suspense fallback={null}>
+//         <Switch>
+//           <Route path="/" exact component={Home} />
+//           <Route path="/about" component={About} />
+//         </Switch>
+//       </Suspense>
+//     </Router>
+//   );
+// };
 
 export default function App() {
+  const [time, setTime] = useState<Date>(new Date());
+  const tick = setInterval(() => setTime(new Date()));
   return (
-    <div className="app-main">
-      <LibVersion />
-      <HelloModal />
-
-      <Divider />
-
-      <RouteExample />
+    <div className="rounded-xl bg-gray-800 flex flex-col p-8 bg-opacity-25">
+      <div className="w-128 h-16 items-center text-white mb-5 flex-shrink-0">
+        <span className="text-4xl">{time.toLocaleTimeString()}</span>
+        <span className="text-lg pl-4">
+          {time.toLocaleString("en-US", {
+            weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          })}
+        </span>
+      </div>
+      <div className="flex-grow text-white">
+        <h2 className="text-xl title-font font-medium mb-3">World Clock</h2>
+        <div className="flex flex-col lg:flex-row w-full lg:space-x-2 space-y-2 lg:space-y-0 mb-2 lg:mb-4">
+          <div className="w-full">
+            <div className="widget w-full p-4 rounded-lg bg-gray-600 bg-opacity-25">
+              <div className="flex flex-row items-center justify-between">
+                <div className="flex flex-col">
+                  <div className="text-xs uppercase font-light">
+                    India
+                  </div>
+                  <div className="font-bold text-xs">
+                    {time.toLocaleTimeString("en-US", {
+                      weekday: "long",
+                      year: "numeric",
+                      month: "long",
+                      day: "numeric",
+                    })}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="w-full">
+            <div className="widget w-full p-4 rounded-lg bg-gray-600 bg-opacity-25">
+              <div className="flex flex-row items-center justify-between">
+                <div className="flex flex-col">
+                  <div className="text-xs uppercase font-light">
+                    India
+                  </div>
+                  <div className="font-bold text-xs">
+                    {time.toLocaleTimeString("en-US", {
+                      weekday: "long",
+                      year: "numeric",
+                      month: "long",
+                      day: "numeric",
+                    })}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          </div>
+      </div>
     </div>
   );
 }
